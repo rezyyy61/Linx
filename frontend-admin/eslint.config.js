8e8fcdaafc943b5a8ec814c8e37215e84eab68c2
rezyyy61 <rezyyy61@gmail.com>
2025-08-14 14:37:11 +0200

    frontend fixed

--- frontend-admin/eslint.config.js
+++ frontend-admin/eslint.config.js
@@ -2,17 +2,31 @@ import js from "@eslint/js";
 import vue from "eslint-plugin-vue";
 import tsParser from "@typescript-eslint/parser";
 import ts from "@typescript-eslint/eslint-plugin";
+import vueParser from "vue-eslint-parser";
 
 export default [
-  ...vue.configs["flat/essential"],
   js.configs.recommended,
+  ...vue.configs["flat/recommended"],
   {
-    files: ["**/*.{ts,vue,js}"],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parser: tsParser,
       parserOptions: { ecmaVersion: "latest", sourceType: "module" }
     },
     plugins: { "@typescript-eslint": ts },
+    rules: {}
+  },
+  {
+    files: ["**/*.vue"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    plugins: { vue, "@typescript-eslint": ts },
     rules: {}
   }
 ];
